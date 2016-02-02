073da31d6fd948a24e7cb0e5df92f12bf4abbfa6
Russ Miles <russ@russmiles.com>
2016-02-02 20:46:41 +0000

    Added configuration server

--- configurationserver/README.md
+++
@@ -1,6 +0,0 @@
-# Sandbox for creating and deployment multiple variants of configuration services
-
-Currently contains:
-
-* **springcloud/redis** : Redis-based Implementation of a Spring Cloud configuration service
-
--- configurationserver/springcloud/configserver/src/main/java/com/russmiles/antifragilesoftware/configurationserver/ConfigServerApplication.java
+++ configurationserver/springcloud/configserver/src/main/java/com/russmiles/antifragilesoftware/configurationserver/ConfigServerApplication.java
@@ -1,17 +1,18 @@
 package com.russmiles.antifragilesoftware.configurationserver;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableConfigServer
-public class ConfigurationServer {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigurationServer.class, args);
+		new SpringApplicationBuilder(ConfigServerApplication.class)
+				.properties("spring.config.name=configserver").run(args);
 	}
+
 }
