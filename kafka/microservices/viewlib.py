721826c798ee1786cc5d4feec480818b33833a49
Sylvain Hellegouarch <sh@defuze.org>
2016-02-11 17:23:25 +0100

    improved the command line for clarity

--- kafka/microservices/viewlib.py
+++ kafka/microservices/viewlib.py
@@ -5,7 +5,11 @@ import asyncio
 from aiohttp import web
 from pykafka import KafkaClient
 
-__all__ = ["run_view", "get_cli_parser"]
+__all__ = ["get_cli_parser", "view", "exit_view"]
+
+# we could probably do better than having a global
+# flag hanging around
+view_running = False
 
 
 async def webserver(loop, view, addr, port):
@@ -32,10 +36,13 @@ async def consume_events(topic, group, addr, callback, delay=0.01):
     If `callback` returns `False`, this will exit
     the consumer immediately.
     """
+    global view_running
     client = KafkaClient(hosts=addr)
     topic = client.topics[topic]
     consumer = topic.get_simple_consumer()
-    while True:
+    
+    view_running = True
+    while view_running:
         message = consumer.consume(block=False)
         if message is not None:
             running = await callback(message)
@@ -43,8 +50,7 @@ async def consume_events(topic, group, addr, callback, delay=0.01):
                 break
         else:
             await asyncio.sleep(delay)
-
-     
+            
 def get_cli_parser():
     """
     Create and return a command line parser
@@ -52,25 +58,35 @@ def get_cli_parser():
     and  processed to extract parameters from the cli.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--topic', dest='topic', action='store',
-                        help='kafka topic to consume from',
-                        required=True)
-    parser.add_argument('--group', dest='group', action='store',
-                        help='kafka group to consume from')
-    parser.add_argument('--broker', dest='broker', action='store',
-                        help='kafka broker address', required=True)
-    parser.add_argument('--addr', dest='addr', action='store',
-                        help='address to bind to',
-                        default='127.0.0.1')
-    parser.add_argument('--port', dest='port', action='store',
-                        help='port to listen on', default=8080)
+
+    messaging = parser.add_argument_group('Messaging parameters')
+    messaging.add_argument('--topic', dest='topic', action='store',
+                            help='kafka topic to consume from',
+                            required=True)
+    messaging.add_argument('--group', dest='group', action='store',
+                            help='kafka group to consume from')
+    messaging.add_argument('--broker', dest='broker', action='store',
+                            help='kafka broker address', required=True)
+    
+    endpoint = parser.add_argument_group('Service parameters')
+    endpoint.add_argument('--addr', dest='addr', action='store',
+                          help='address to bind to',
+                          default='127.0.0.1')
+    endpoint.add_argument('--port', dest='port',
+                          action='store', type=int,
+                          help='port to listen on', default=8080)
+    endpoint.add_argument('--name', dest='name', action='store',
+                          help='published service name')
+    endpoint.add_argument('--id', dest='id', action='store',
+                          help='published service id')
+    endpoint.add_argument('--tags', dest='tags', action='store',
+                          help='published service tags', nargs='*')
     return parser
 
 
-def run_view(loop, args, view, event_handler):
+def view(loop, args, view, event_handler):
     """
-    Run the view and event consumer asynchronously
-    within the given main asyncio loop.
+    Setup the view and event consumer.
     """
     coroutines = [
         consume_events(topic=args.topic.encode('utf-8'),
@@ -79,5 +95,14 @@ def run_view(loop, args, view, event_handler):
                        callback=event_handler),
         webserver(loop, view, addr=args.addr, port=args.port)
     ]
-    loop.run_until_complete(asyncio.wait(coroutines, loop=loop))
+    return asyncio.wait(coroutines, loop=loop)
     
+
+async def exit_view():
+    """
+    Simply turn the global running flag to `False`
+    so that the event consuming coroutine
+    can terminate.
+    """
+    global view_running
+    view_running = False
