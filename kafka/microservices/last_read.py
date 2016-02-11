9a2f20120cd79c1257f8f284a10ac4c49aacc4e0
Sylvain Hellegouarch <sh@defuze.org>
2016-02-11 17:24:49 +0100

    support service discovery and reworked the pipeline

--- kafka/microservices/last_read.py
+++ kafka/microservices/last_read.py
@@ -8,18 +8,29 @@ To run it:
 
 .. code-block:: python
 
-   $ python --broker KAFKA_IP:9092 --topic my-topic --addr 127.0.0.1 --port 8080 
+   $ python last_read.py --topic mytopic --broker <BROKER_ADDR>:9092 --port 8080 --name lastread --id lastread1 --tags book last
 
 This will listen for HTTP request on `127.0.0.1:8080`
 and will return a JSON encoded list of book
 documents.
+
+The service will be automatically registered to the local consul
+agent (assuming running localhost:8500) using the
+name, id and tags provided
+
+When the process is terminated, the service is deregistered
+automatically.
 """
 import json
 
 from aiohttp import web
 
+from discolib import service
+
+
 # internal state
 bookshelf = []
+
 
 async def bookshelf_view(request):
     """
@@ -42,18 +53,40 @@ async def event_handler(message):
     
 if __name__ == '__main__':
     import asyncio
+    import signal
     
-    from viewlib import get_cli_parser, run_view
+    from viewlib import get_cli_parser, view, exit_view
+    from discolib import service
 
+    # let's parse the service instance settings
+    args = get_cli_parser().parse_args()
+
+    # everything will be dealt in this mainloop
     loop = asyncio.get_event_loop()
-    try:
-        run_view(loop,
-                 get_cli_parser().parse_args(),
-                 view=bookshelf_view,
-                 event_handler=event_handler
+
+    # hook onto the usual termination signal
+    # to deregister the service properly
+    loop.add_signal_handler(signal.SIGINT,
+                            asyncio.ensure_future,
+                            exit_view())
+    
+    # run the view as a service until we exit it
+    # the service will be advertised to the
+    # discovery service and deregister once
+    # we exit
+    loop.run_until_complete(
+        service(
+            view(
+                loop,
+                args,
+                view=bookshelf_view,
+                event_handler=event_handler
+            ),
+            id=args.id,
+            name=args.name,
+            port=args.port,
+            address=args.addr,
+            tags=args.tags
         )
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        loop.close()
+    )
+        
